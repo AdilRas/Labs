7c3ea6fb06af5ff8b7a20c1fa08ec223ddfedd74
adilras <ar7463@gmail.com>
2017-09-15 11:13:56 -0500

    Lab23 file

---
+++ src/Lab23ast.java
@@ -0,0 +1,15 @@
+// Lab23ast.java
+// This is the starting version of the "Perfect Number" program. 
+
+
+import java.util.*;
+
+
+public class Lab23ast
+{
+	public static void main (String args[]) 
+	{   
+		
+	}
+       
+}
