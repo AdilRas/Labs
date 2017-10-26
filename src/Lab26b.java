88e115ada27a0a94a1728c5e754df1af0e3aaba8
adilras <ar7463@gmail.com>
2017-10-26 12:54:57 -0500

    Lab23 file

--- src/Lab26b.java
+++ src/Lab26b.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Lab26b {
         public static void main (String args[])
@@ -18,9 +20,8 @@ class Knight {
         private int rowPos;				// current row position of the knight
         private int colPos;				// current col position of the knight
         private int moves;				// number of location visited by the knight
-
-	/*								USED WITH THE 100 POINT VERSION
-	final private int ACCESS[][] = {{0,0,0,0,0,0,0,0,0,0,0,0},
+	    final private int ACCESS[][] = {
+	                                {0,0,0,0,0,0,0,0,0,0,0,0},
  						 			{0,0,0,0,0,0,0,0,0,0,0,0},
      					 			{0,0,2,3,4,4,4,4,3,2,0,0},
      					 			{0,0,3,4,6,6,6,6,4,3,0,0},
@@ -31,28 +32,42 @@ class Knight {
      					 			{0,0,3,4,6,6,6,6,4,3,0,0},
      					 			{0,0,2,3,4,4,4,4,3,2,0,0},
      					 			{0,0,0,0,0,0,0,0,0,0,0,0},
-     					 			{0,0,0,0,0,0,0,0,0,0,0,0}};
-	*/
+     					 			{0,0,0,0,0,0,0,0,0,0,0,0}
+	    };
 
         // constructor used to initializes the data attributes
         public Knight() {
-
+            this(1,1);
+            getStart();
+        }
+        public Knight(int startRow, int startCol) {
+            this.startRow = startRow;
+            this.startCol = startCol;
+            this.rowPos = 0;
+            this.colPos = 0;
+            this.moves = 0;
         }
 
-        // input method to get starting row and col from keyboard entry
+    // input method to get starting row and col from keyboard entry
         public void getStart() {
-
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter starting row ==> ");
+            this.startRow = sc.nextInt();
+            System.out.print("Enter starting col ==> ");
+            this.startCol = sc.nextInt();
         }
 
 
         // displays the chessboard after the tour is concluded
         public void displayBoard() {
 
+
         }
 
         // computes the next available knight's move.  Alters RowPos and ColPos and
         // returns true if move is possible, otherwise returns false
         private boolean getMove() {
+
             return false;
         }
 
